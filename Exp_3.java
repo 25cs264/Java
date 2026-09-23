198a69931a08854820ec76d2bb79ba63b6179570
25cs264 <25cs264@kpriet.ac.in>
2026-09-23 22:28:37 +0530

    Create Exp_3.java

---
+++ Exp_3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Exp_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Student ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();  
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Department: ");
+        String department = sc.nextLine();
+        System.out.print("Enter Age: ");
+        int age = sc.nextInt();
+        System.out.print("Enter Percentage: ");
+        double percentage = sc.nextDouble();
+        System.out.println("\n----- Student Admission Details -----");
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage + "%");
+        sc.close();
+    }
+}
